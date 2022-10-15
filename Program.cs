30424a260d7fd88192f08e176d6382b810c02282
Dmitry <roddg86@gmail.com>
2022-10-15 21:14:16 +0900

    Решение Задача 6

--- Program.cs
+++ Program.cs
@@ -50,3 +50,27 @@ else
     Console.WriteLine($"Максимальное число: {firstNumber}");
 }
 */
+
+/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+/*
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int even = number % 2;
+
+if (even == 0) 
+{
+    Console.WriteLine($"Число: {number} четное");
+}
+else
+{
+    Console.WriteLine($"Число: {number} не четное");
+}
+*/
