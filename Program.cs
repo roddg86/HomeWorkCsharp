9cd5b416e114b24e1e39c45b1509e6627791ce0f
Dmitry <roddg86@gmail.com>
2022-10-15 21:24:16 +0900

    Задача 8 Решение

--- Program.cs
+++ Program.cs
@@ -74,3 +74,22 @@ else
     Console.WriteLine($"Число: {number} не четное");
 }
 */
+
+/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+/*
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int res = 1;
+while (res < number)
+{
+Console.WriteLine(res+1);
+res+= 2;
+}
+*/
