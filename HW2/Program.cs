857e28f55caad3dce0a81e887e7e73082fbc4076
Dmitry <roddg86@gmail.com>
2022-10-18 21:02:42 +0900

    Задача 10. Решена

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SecondDigit(int number)
+{
+    int ed = number % 100;
+    int result = ed / 10;
+
+    return result;
+}
+
+int newNum = SecondDigit(number);
+
+Console.WriteLine($"New version of {number} is {newNum}");
