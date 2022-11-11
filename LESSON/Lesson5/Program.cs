a3d2e9d35a4806ac372a4967a9b6260ec37d024e
Dmitry <110647769+roddg86@users.noreply.github.com>
2022-11-11 17:08:27 +0900

    task solved

--- LESSON/Lesson5/Program.cs
+++ LESSON/Lesson5/Program.cs
@@ -11,7 +11,7 @@ int[] CreateRandomArray(int size, int minValue, int maxValue)
     return array;
 }
 
-//метод выводит массив на экран
+// метод выводит массив на экран
 void ShowArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
@@ -20,7 +20,16 @@ void ShowArray(int[] array)
     Console.WriteLine();
 }
 
+// метод находит сумму отрицательных элементов массива
+int GetNegativeSum(int[] array)
+{
+    int sum = 0;
 
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] < 0) sum += array[i];
+
+    return sum;
+}
 
 Console.Write("Input a number of elements: ");
 int n = Convert.ToInt32(Console.ReadLine());
@@ -28,3 +37,9 @@ Console.Write("Input a min possible value: ");
 int min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a max possible value: ");
 int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(n, min, max);
+ShowArray(myArray);
+
+int negativeSum = GetNegativeSum(myArray);
+Console.WriteLine("Sum of negative elements is " + negativeSum);
