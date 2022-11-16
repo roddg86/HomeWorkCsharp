7fd2cd687b6d7f430f5c57b1c4f378007e9f886d
Dmitry <roddg86@gmail.com>
2022-11-16 21:26:19 +0900

    tasks lesson 6 solved

--- LESSON/Lesson6/Program.cs
+++ LESSON/Lesson6/Program.cs
@@ -1,7 +1,7 @@
 ﻿/* Напишите программу, которая перевернёт одномерный массив 
 (последний элемент будет на первом месте, а первый - на последнем и т.д.) */
 
-// метод генерирует массив случайных чисел
+/* // метод генерирует массив случайных чисел
 int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
     int[] array = new int[size];
@@ -20,7 +20,7 @@ void ShowArray(int[] array)
 
     Console.WriteLine();
 }
-
+ */
 /* // метод разворачивает массив
 void ReverseArray(int[] array)
 {
@@ -52,7 +52,7 @@ ShowArray(myArray);
 двух предыдущих чисел
  */
 
-int[] Fib(int size, int a, int b)
+/* int[] Fib(int size, int a, int b)
 
 {
     int[] array = new int[size];
@@ -72,14 +72,47 @@ Console.Write("Input a max possible value: ");
 int max = Convert.ToInt32(Console.ReadLine());
 
 int[] myArray = CreateRandomArray(n, min, max);
-ShowArray(myArray);
-ShowArray(Fib(n, min, max));
+//ShowArray(myArray);
+ShowArray(Fib(n, min, max)); */
 
 /* 
 Напишите программу, которая будет создавать копию заданного массива
 с помощью поэлементного копирования. */
 
+/* int[] Copy(int[] array)
+{
+    int[] copyArary = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+        copyArary[i] = array[i];
+    return copyArary;
+}
+
+Console.Write("Input a number of elements: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(n, min, max);
+ShowArray(myArray);
+ShowArray(Copy(myArray));
+ */
 
 /* 
 Напишите программу, которая принимает на вход три числа и проверяет, 
 может ли существовать треугольник с сторонами такой длины. */
+
+bool Triangle(int a, int b, int c)
+{
+    return a + b > c && a + c > b && b + c > a;
+}
+
+Console.Write("Input a number of elements: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Triangle(n, min, max));
