aaa4d24d5dc1aa2b9c8eee8bb9158bde7e50834c
Dmitry <roddg86@gmail.com>
2022-11-19 22:57:46 +0900

    task 52 solved

--- HW7/Program.cs
+++ HW7/Program.cs
@@ -125,7 +125,7 @@ int columns = Convert.ToInt32(Console.ReadLine());
 
 SearchElement(myArray, rows, columns);
  */
- 
+
 /* Задача 52. Задайте двумерный массив из целых чисел. 
 Найдите среднее арифметическое элементов в каждом столбце.
 
@@ -136,3 +136,65 @@ SearchElement(myArray, rows, columns);
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
  */
 
+/* // метод генерирует двумерный массив заполненный случайнми числами
+int[,] CreateRandom2Array(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+// метод выводит двумерный массив на экран
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + "  ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[] SearchAverage(int[,] array)
+{
+    double[] arrayAverage = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            arrayAverage[j] += array[i, j];
+        }
+        arrayAverage[j] /= array.GetLength(0);
+        //Console.WriteLine($"Cреднее арифметическое элементов столбца {j + 1} = {Math.Round(arrayAverage[j], 1)}");
+    }
+    return arrayAverage;
+}
+
+// метод выводит массив на экран
+void ShowArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(Math.Round(array[i], 2) + " ");
+
+    Console.WriteLine();
+}
+
+Console.Write("Input a number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] myArray = CreateRandom2Array(m, n, min, max);
+Show2dArray(myArray);
+ShowArray(SearchAverage(myArray)); */
